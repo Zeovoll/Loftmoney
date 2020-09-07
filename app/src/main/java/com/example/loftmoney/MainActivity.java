ba85ceff662042f33ccf7f22f1203146abbfec45
User <ardo.bo@mail.ru>
2020-09-07 19:11:05 +0300

    First commit

---
+++ app/src/main/java/com/example/loftmoney/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.loftmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newActivity = new Intent(getApplicationContext(), SecondActivity.class);
+//                newActivity.putExtra(name:"COST_ID", value:1)
+                startActivity(newActivity);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/loftmoney/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.loftmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+//        int costID =getIntent().getExtra(name:"COST_ID",defaultValue:0);
+//        Log.e(tag:"TAG",msg:"COST ID")
+    }
+}
